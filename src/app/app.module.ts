4cb466431f4db076fa03c42ed18835226ea24e76
AyoubHdhili <hdhili.ayoub@esprit.tn>
2023-10-14 00:22:13 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ProductsComponent } from './products/products.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
+import { TeamComponent } from './team/team.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    PricingComponent,
+    ProductsComponent,
+    ProjectsComponent,
+    ServicesComponent,
+    TeamComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule,
