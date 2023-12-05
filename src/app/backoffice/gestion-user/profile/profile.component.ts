c04a3e710cd4933f532648720063ecd5542a6f3b
AyoubHdhili <hdhili.ayoub@esprit.tn>
2023-12-05 16:19:29 +0100

    update profile toast

--- src/app/backoffice/gestion-user/profile/profile.component.ts
+++ src/app/backoffice/gestion-user/profile/profile.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {User} from "../../../shared/models/User";
 import {UserService} from "../../../shared/services/userService/user.service";
 import {ActivatedRoute} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-profile',
@@ -12,7 +13,7 @@ export class ProfileComponent implements OnInit{
   user:User = new User();
   id : number = 0;
 
-  constructor(private userService:UserService, private _activated: ActivatedRoute) {
+  constructor(private userService:UserService, private _activated: ActivatedRoute, private toast: ToastrService) {
   }
 
   ngOnInit(): void {
@@ -26,7 +27,10 @@ export class ProfileComponent implements OnInit{
 
   update() {
     this.userService.updateUser(this.id,this.user).subscribe((res) =>{
-      console.log(res);
+      this.toast.success("User updated successfully !!", "",{
+        timeOut: 5000,
+        positionClass:'toast-top-center'
+      });
     })
   }
 }
